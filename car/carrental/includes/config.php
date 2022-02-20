ad06c32c8d1af924b376d7489bd3c7c125034b7c
saiprasadrapeti <93754879+saiprasadrapeti@users.noreply.github.com>
2022-02-20 22:27:11 +0530

    Update config.php

--- car/carrental/includes/config.php
+++ car/carrental/includes/config.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_USER','admin');
+define('DB_PASS','saiprasadrapeti');
 define('DB_NAME','carrental');
 // Establish database connection.
 try
